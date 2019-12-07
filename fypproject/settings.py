7c9677cdbd60c4b4eaee397e3f775a438ff30396
Umair Hashmi <princehashmi7147@mail.com>
2019-12-08 00:51:36 +0500

    Initial commit

--- fypproject/settings.py
+++ fypproject/settings.py
@@ -146,11 +146,17 @@ DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+
 #STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+os.path.join(BASE_DIR, 'static'),
 )
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
